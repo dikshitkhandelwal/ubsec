5f6018bc151cfe5bf0f94be8b7f13c41c73e7d7d
dikshitkhandelwal <dikshitkhandelwal1@gmail.com>
2024-02-23 18:08:10 -0500

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import Contact from "./Contact";
 import Create from "./Create";
 import Home04 from "./Home04";
-import Login from "./Login";
-import SignUp from "./SignUp";
 import Team from "./Wallet";
 import HackFest24Info from "./HackFest";
 
@@ -11,8 +9,6 @@ import HackFest24Info from "./HackFest";
 const routes = [
   { path: '/', component: <Home04 />},
   { path: '/wallet', component: <Team />},
-  { path: '/login', component: <Login />},
-  { path: '/signup', component: <SignUp />},
   { path: '/create', component: <Create />},
   { path: '/contact', component: <Contact />},
   { path: '/HackFest24Info', component: <HackFest24Info />},
