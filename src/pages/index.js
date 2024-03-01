1aed05e04a129cb14fd7c62274e8bfcb6126c68e
dikshitkhandelwal <dikshitkhandelwal1@gmail.com>
2024-02-29 20:23:25 -0500

    removed the extra homepages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
 import Contact from "./Contact";
-import Home01 from "./Home01";
-import Home02 from "./Home02";
-import Home03 from "./Home03";
-import Home04 from "./Home04";
-import Home05 from "./Home05";
+// import Home01 from "./Home01";
+// import Home02 from "./Home02";
+// import Home03 from "./Home03";
+// import Home04 from "./Home04";
+// import Home05 from "./Home05";
 import Home06 from "./Home06";
 import Eboard from "./Eboard";
 import About from "./About";
 import Spring24 from "./Events/Spring24";
 
 const routes = [
-  { path: '/', component: <Home05 />},
-  { path: '/home-v2', component: <Home02 />},
-  { path: '/home-v3', component: <Home03 />},
-  { path: '/home-v4', component: <Home04 />},
-  { path: '/home-v5', component: <Home01 />},
-  { path: '/home-v6', component: <Home06 />},
+  { path: '/', component: <Home06 />},
+  // { path: '/home-v2', component: <Home02 />},
+  // { path: '/home-v3', component: <Home03 />},
+  // { path: '/home-v4', component: <Home04 />},
+  // { path: '/home-v5', component: <Home05 />},
+  // { path: '/home-v6', component: <Home01 />},
   { path: '/contact', component: <Contact />},
   {path: '/eboard', component: <Eboard />},
   {path: '/events/spring-2024', component: <Spring24 />},
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,38 +3,38 @@ const menus = [
         id: 1,
         name: 'Home',
         links: '/',
-        namesub: [
-            {
-                id: 1,
-                sub: 'Home 01',
-                links: '/'
-            },
-            {
-                id: 2,
-                sub: 'Home 02',
-                links: '/home-v2'
-            },
-            {
-                id: 3,
-                sub: 'Home 03',
-                links: '/home-v3'
-            },
-            {
-                id: 4,
-                sub: 'Home 04',
-                links: '/home-v4'
-            },
-            {
-                id: 5,
-                sub: 'Home 05',
-                links: '/home-v5'
-            },
-            {
-                id: 6,
-                sub: 'Home 06',
-                links: '/home-v6'
-            },
-        ]
+        // namesub: [
+        //     {
+        //         id: 1,
+        //         sub: 'Home 01',
+        //         links: '/'
+        //     },
+        //     {
+        //         id: 2,
+        //         sub: 'Home 02',
+        //         links: '/home-v2'
+        //     },
+        //     {
+        //         id: 3,
+        //         sub: 'Home 03',
+        //         links: '/home-v3'
+        //     },
+        //     {
+        //         id: 4,
+        //         sub: 'Home 04',
+        //         links: '/home-v4'
+        //     },
+        //     {
+        //         id: 5,
+        //         sub: 'Home 05',
+        //         links: '/home-v5'
+        //     },
+        //     {
+        //         id: 6,
+        //         sub: 'Home 06',
+        //         links: '/home-v6'
+        //     },
+        // ]
     },
     {
         id: 2,
